64e6c4f6b786c986da255243051a21ad3cc10dd1
Benjamin Hodgens <ben@hodgens.net>
2026-05-21 16:08:59 -0600

    feat(flutter): initialize providers on app startup

--- ui/flutter_ui/lib/features/home/home_screen.dart
+++ ui/flutter_ui/lib/features/home/home_screen.dart
@@ -1,31 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../theme/colors.dart';
 import '../../theme/effects.dart';
 import '../../widgets/tab_bar.dart';
+import '../../providers/providers.dart';
 import 'tab_content.dart';
 
 /// Home tab enum - 4 tabs as specified
 enum HomeTab { chat, sessions, tasks, agents }
 
 /// Home screen - main app screen with top tab navigation
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends ConsumerState<HomeScreen> {
   HomeTab _selectedTab = HomeTab.chat;
 
   final List<String> _tabLabels = ['chat', 'sessions', 'tasks', 'agents'];
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize providers on app start
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      ref.read(sessionProvider.notifier).loadSessions();
+      ref.read(taskProvider.notifier).loadTasks();
+      ref.read(agentProvider.notifier).loadAgents();
+      // Chat provider is initialized when first message is sent
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: CyberpunkColors.black,
       body: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           gradient: CyberpunkEffects.angularGradient,
         ),
         child: SafeArea(
